db55cd6ecf2e826af204460f2f4a67ea4158b723
Marharyta Trofymenko <Marharyta_Trofymenko@epam.com>
2020-05-05 22:05:22 +0300

    fix: auth path

--- src/Sloth.Auth/AuthenticationHandler/BaseAuthHandler.cs
+++ src/Sloth.Auth/AuthenticationHandler/BaseAuthHandler.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using ISystemClock = Microsoft.AspNetCore.Authentication.ISystemClock;
 
 namespace Sloth.Auth.AuthenticationHandler
 {
